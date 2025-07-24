eb4036a5a6bfd4e465a7fcdc5c81e89488a180be
DeepPasnani <168401187+DeepPasnani@users.noreply.github.com>
2025-07-24 08:37:27 +0000

    Write a program to demonstrate the concepts of Call by value and Call by reference.

--- practical_1.c
+++ practical_1.c
@@ -23,13 +23,13 @@ int main()
     int a = 1; 
     int b = 2;
     
-    printf("Before swapping (CallByValue): %d, %d \n", a, b);
+    printf("Before swapping ( CallByValue ): %d, %d \n", a, b);
     swapVal(a, b);
-    printf("After swapping (CallByValue): %d, %d \n", a, b);
+    printf("After swapping ( CallByValue ): %d, %d \n", a, b);
 
-    printf("Before swapping (CallByReference): %d, %d \n", a, b);
+    printf("Before swapping ( CallByReference ): %d, %d \n", a, b);
     swapRef(&a, &b);
-    printf("After swapping (CallByReference): %d, %d \n", a, b);
+    printf("After swapping ( CallByReference ): %d, %d \n", a, b);
 
     return 0;
 }

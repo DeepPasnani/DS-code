bc84151b1a816ca0f9573c3bb6dbc99fb3cd35f4
DeepPasnani <168401187+DeepPasnani@users.noreply.github.com>
2025-09-11 07:07:37 +0000

    Write a program to implement stack using linked list.

---
+++ sl_practical_2_a.c
@@ -0,0 +1,64 @@
+// Stack using Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    newNode->data = val;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    struct Node* temp = top;
+    top = top->next;
+    free(temp);
+}
+
+int peek() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    return top->data;
+}
+
+void display() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+    struct Node* temp = top;
+    while (temp) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    printf("Top element is %d\n", peek());
+    pop();
+    display();
+    printf("Top element is %d\n", peek());
+    return 0;
+}
